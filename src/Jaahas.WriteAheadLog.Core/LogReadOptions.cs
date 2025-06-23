e3653fa97265a0deb4348b6e8e5196d2c5bf5f77
Graham Watts <grahamcharleswatts@gmail.com>
2025-06-23 12:44:19 +0300

    Refactor LogReadOptions to use LogPosition
    Replaces separate SequenceId and Timestamp parameters with a single LogPosition parameter in LogReadOptions. Updates XML documentation to reflect this change and removes remarks about precedence.

--- src/Jaahas.WriteAheadLog.Core/LogReadOptions.cs
+++ src/Jaahas.WriteAheadLog.Core/LogReadOptions.cs
@@ -3,11 +3,9 @@ namespace Jaahas.WriteAheadLog;
 /// <summary>
 /// Options for reading from the Write-Ahead Log (WAL).
 /// </summary>
-/// <param name="SequenceId">
-///   The sequence ID to start reading from.
-/// </param>
-/// <param name="Timestamp">
-///   The timestamp to start reading from, measured in ticks.
+/// <param name="Position">
+///   The position in the log to start reading from. This can be specified using either a sequence
+///   ID or a timestamp.
 /// </param>
 /// <param name="Limit">
 ///   The maximum number of messages to read.
@@ -17,8 +15,4 @@ namespace Jaahas.WriteAheadLog;
 ///   the latest segment when it reaches the end of the log stream. Otherwise, the operation
 ///   will complete when is reaches the end of the latest segment.
 /// </param>
-/// <remarks>
-///   If both <paramref name="SequenceId"/> and <paramref name="Timestamp"/> are specified,
-///   <paramref name="Timestamp"/> takes precedence.
-/// </remarks>
-public readonly record struct LogReadOptions(ulong SequenceId = 0UL, long Timestamp = -1, long Limit = -1, bool WatchForChanges = false);
+public readonly record struct LogReadOptions(LogPosition Position = default, long Limit = -1, bool WatchForChanges = false);

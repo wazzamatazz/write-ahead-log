6180a0224f860b0dadd27df4b52caaeca8fd5484
Graham Watts <grahamcharleswatts@gmail.com>
2025-06-28 21:21:07 +0300

    Remove streaming writes from IWriteAheadLog (#14)
    * Remove batch WriteAsync method from IWriteAheadLog
    
    Deleted the interface method for writing multiple log entries asynchronously. This simplifies the IWriteAheadLog contract.
    
    * Remove batch WriteAsync method from FileWriteAheadLog
    
    * Add streaming write support to GrpcWriteAheadLog
    
    Introduces the UseStreamingWrites option to GrpcWriteAheadLogOptions and updates GrpcWriteAheadLog to support both streaming and non-streaming write operations internally.

--- src/Jaahas.WriteAheadLog.Grpc/GrpcWriteAheadLogOptions.cs
+++ src/Jaahas.WriteAheadLog.Grpc/GrpcWriteAheadLogOptions.cs
@@ -1,7 +1,22 @@
 namespace Jaahas.WriteAheadLog.Grpc;
 
+/// <summary>
+/// Options for <see cref="GrpcWriteAheadLog"/>.
+/// </summary>
 public class GrpcWriteAheadLogOptions() {
 
+    /// <summary>
+    /// The name of the remote log to connect to.
+    /// </summary>
     public string? LogName { get; set; }
+    
+    /// <summary>
+    /// Specifies whether to use streaming writes for writing to the log.
+    /// </summary>
+    /// <remarks>
+    ///   Streaming writes use bidirectional streaming to write log entries whereas non-streaming
+    ///   writes use unary RPCs for each log entry.
+    /// </remarks>
+    public bool UseStreamingWrites { get; set; } = true;
 
 }

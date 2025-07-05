6fa7fd697731dff613685b9e483a4f39a9155d58
Graham Watts <grahamcharleswatts@gmail.com>
2025-07-05 07:53:04 +0300

    Add options post-configuration for WriteAheadLog
    Introduces WriteAheadLogPostConfigureOptions to ensure the Name property of WriteAheadLogOptions matches the options instance name. Adds an extension method to OptionsBuilder to register this post-configuration automatically.

---
+++ src/Jaahas.WriteAheadLog/DependencyInjection/OptionsBuilderExtensions.cs
@@ -0,0 +1,37 @@
+using Jaahas.WriteAheadLog.DependencyInjection.Internal;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace Jaahas.WriteAheadLog.DependencyInjection;
+
+/// <summary>
+/// Extensions for <see cref="OptionsBuilder{TOptions}"/>
+/// </summary>
+public static class OptionsBuilderExtensions {
+
+    /// <summary>
+    /// Adds default post-configuration for the write-ahead log <typeparamref name="TOptions"/> to
+    /// the <see cref="OptionsBuilder{TOptions}"/>.
+    /// </summary>
+    /// <param name="builder">
+    ///   The <see cref="OptionsBuilder{TOptions}"/> to which the post-configuration is added.
+    /// </param>
+    /// <typeparam name="TOptions">
+    ///   The write-ahead log options type.
+    /// </typeparam>
+    /// <returns>
+    ///   The <see cref="OptionsBuilder{TOptions}"/> with the post-configuration added.
+    /// </returns>
+    public static OptionsBuilder<TOptions> AddDefaultPostConfigureOptions<TOptions>(
+        this OptionsBuilder<TOptions> builder
+    ) where TOptions : WriteAheadLogOptions, new() {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<TOptions>, WriteAheadLogPostConfigureOptions<TOptions>>());
+        
+        return builder;
+    }
+
+}
